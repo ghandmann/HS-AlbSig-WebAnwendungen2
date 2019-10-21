8219975fede3df4299d3c5f76e5ba31338689916
Sven Eppler <sven.eppler@sodgeit.de>
2019-10-21 21:50:54 +0200

    remove default routers, add guestbook router

--- Beispiele/Node.js/GuestbookAPI/app.js
+++ Beispiele/Node.js/GuestbookAPI/app.js
@@ -3,8 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var guestbookRouter = require('./routes/guestbook');
 
 var app = express();
 
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', guestbookRouter);
 
 module.exports = app;
