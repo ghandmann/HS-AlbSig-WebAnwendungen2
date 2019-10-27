75c508acd368cc7e9918fa5be46a62d7dbf86f92
Sven Eppler <git@sveneppler.de>
2019-10-27 13:39:14 +0100

    include swagger-ui

---
+++ Beispiele/Node.js/GuestbookAPI/README.md
@@ -0,0 +1,23 @@
+# Guestbook API
+
+This node.js Express project is an example for a simple Guestbook API including a JavaScript based HTML5 client.
+
+## Getting Started
+
+Before running this example you need to install the dependencies with
+
+```
+$ npm install
+```
+
+afterwards you can run the buildin express webserver with
+
+```
+$ npm start
+```
+
+You can visit the guestbook via it's included HTML5 frontend on http://localhost:3000/
+
+## API Documentation
+
+The project includes a Swagger-UI Documentation on http://localhost:3000/swagger.html
--- Beispiele/Node.js/GuestbookAPI/app.js
+++ Beispiele/Node.js/GuestbookAPI/app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const swaggerUi = require('swagger-ui-dist');
 
 var indexRouter = require('./routes/indexRouter');
 var apiRouter = require('./routes/guestbookApiRouter');
 
 var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +18,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/', apiRouter);
+
+// Deliver the swagger-ui relevant Javascript libs
+app.use('/swagger-ui/', express.static(swaggerUi.getAbsoluteFSPath()));
 
 module.exports = app;
