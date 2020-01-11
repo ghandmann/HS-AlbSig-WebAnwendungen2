5eadbc5ae3e8c337eb3194eb531c9c70adc8a532
Sven Eppler <git@sveneppler.de>
2020-01-11 21:50:03 +0100

    use imagemagick to identify images

--- Beispiele/Node.js/Microservices/ImageValidationService/app.js
+++ Beispiele/Node.js/Microservices/ImageValidationService/app.js
@@ -1,9 +1,12 @@
 var amqp = require('amqplib/callback_api');
+const fs = require('fs');
+const imagemagick = require('imagemagick');
 
 console.log(" [*] ImageFingerprintService starting...");
 
 const incomingQueue = 'ImageUploaded';
-const watermarkQueue = "ImageFingerprint";
+const fingerprintQueue = 'ImageFingerprint';
+const uploadFailedQueue = 'ImageFailed';
 
 amqp.connect('amqp://localhost', function(connectError, connection) {
     if (connectError) {
@@ -18,7 +21,11 @@ amqp.connect('amqp://localhost', function(connectError, connection) {
             durable: false
         });
 
-        channel.assertQueue(watermarkQueue, {
+        channel.assertQueue(fingerprintQueue, {
+            durable: false,
+        });
+
+        channel.assertQueue(uploadFailedQueue, {
             durable: false,
         });
 
@@ -30,13 +37,40 @@ amqp.connect('amqp://localhost', function(connectError, connection) {
     });
 });
 
-function createImageFingerprint(msg, channel) {
-    console.log(" [x] Received raw message=%s", msg.content.toString());
-    console.log(" [x] Validating image...");
+function createImageFingerprint(incomingMessage, channel) {
+    const json = incomingMessage.content.toString();
+    console.log(" [x] Received raw message=%s", json);
+    const msg = JSON.parse(json);
+    console.log(" [x] Validating imageId='" + msg.imageId + "'");
 
     // Artificiall delay processing of the message by using a timeout
     setTimeout(() => {
-        console.log(" [x] Image validated.");
-        channel.sendToQueue(watermarkQueue, Buffer.from("Validate the image!"));
-    }, 100);
+        imagemagick.identify(msg.uploadDestination, (err, metadata) => {
+            if(err) {
+                console.log(" [E] Image with imageId='" + msg.imageId + "' seems not to be an image. Error=" + err);
+                // Delete the already uploaded image from disk
+                fs.unlinkSync(msg.uploadDestination);
+                // Add error to message
+                msg.validationError = err.toString();
+                // Send a notification that the upload failed
+                channel.sendToQueue(uploadFailedQueue, Buffer.from(JSON.stringify(msg)));
+            }
+            else {
+                // read some of the metadata gathered by ImageMagick
+                const imageInfo = {
+                    format: metadata.format,
+                    width: metadata.width, 
+                    height: metadata.height
+                };
+
+                console.log(" [x] Information about imageId='" + msg.imageId + "': " + JSON.stringify(imageInfo));
+
+                // Attach image information to msg
+                msg.imageInformation = imageInfo;
+                
+                // Foward processing to fingerprint service
+                channel.sendToQueue(fingerprintQueue, Buffer.from(JSON.stringify(msg)));
+            }
+        });
+    }, 1000);
 }
