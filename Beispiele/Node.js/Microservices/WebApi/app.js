e491d8d172fb6e04d35499a3c602dff9646f9bfd
Sven Eppler <git@sveneppler.de>
2020-01-11 10:00:37 +0100

    accept file uploads with busboy

--- Beispiele/Node.js/Microservices/WebApi/app.js
+++ Beispiele/Node.js/Microservices/WebApi/app.js
@@ -2,17 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var busboy = require('connect-busboy');
 
 var indexRouter = require('./routes/index');
+var imageRouter = require('./routes/images');
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(busboy({ highWaterMark: 4*1024*1024 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/images', imageRouter);
 
 module.exports = app;
---
+++ Beispiele/Node.js/Microservices/WebApi/routes/images.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+const { uuid } = require('uuidv4');
+const fs = require('fs');
+
+let channel = null;
+const storageBasePath = '/tmp/shared';
+
+const rabbitMq = require('amqplib').connect('amqp://localhost')
+    .then(conn => conn.createChannel())
+    .then((ch) => channel = ch);
+
+/* GET home page. */
+router.get('/:imageId', (req, res) => {
+  res.send("Images");
+});
+
+router.post('/upload', (req, res) => {
+    // Pipe the request-stream into the busboy requsest parser
+    req.pipe(req.busboy);
+
+    // Whenever busboy detects a file-upload field
+    req.busboy.on('file', (fieldname, file, filename) => {
+        console.log(" * Processing file upload for '" + filename + "'");
+
+        var newImageId = uuid();
+        
+        const uploadDestination = storageBasePath + '/in-flight/' + newImageId;
+        const fstream = fs.createWriteStream(uploadDestination);
+        file.pipe(fstream);
+
+        fstream.on('close', () => {
+            console.log(" * File upload of '" + filename + "' done, uploadDestination='" + uploadDestination + "'");
+
+            publishImageUploadedEvent(newImageId, uploadDestination);
+        });
+    });
+
+    // When busboy is done with the request parsing
+    req.busboy.on('finish', () => {
+        res.sendStatus(200);
+    })
+});
+
+function publishImageUploadedEvent(imageId, uploadDestination) {
+    const imageUploadedEvent = {
+        imageId: imageId,
+        uploadDestination: uploadDestination,
+        timestamp: new Date().toISOString(),
+    };
+
+    channel.sendToQueue("ImageUploaded", Buffer.from(JSON.stringify(imageUploadedEvent)));
+}
+
+
+module.exports = router;
