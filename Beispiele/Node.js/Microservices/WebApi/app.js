ca35e4edafe88c0d52f8b52c43796758b6ac15fa
Sven Eppler <git@sveneppler.de>
2020-01-12 00:21:43 +0100

    create needed folders

--- Beispiele/Node.js/Microservices/WebApi/app.js
+++ Beispiele/Node.js/Microservices/WebApi/app.js
@@ -3,6 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var busboy = require('connect-busboy');
+const fs = require('fs');
+
+// make sure the required folders exist
+fs.mkdir('/tmp/shared/done', { recursive: true }, () => console.log("* Created /tmp/shared/done"));
+fs.mkdir('/tmp/shared/in-flight', { recursive: true }, () => console.log("* Created /tmp/shared/in-flight"));
 
 var indexRouter = require('./routes/index');
 var imageRouter = require('./routes/images');
