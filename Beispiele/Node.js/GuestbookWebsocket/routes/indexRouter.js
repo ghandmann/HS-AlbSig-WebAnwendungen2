798449217b01470c58bc23790f3251589bae0617
Sven Eppler <sven.eppler@sodgeit.de>
2022-01-16 22:30:25 +0100

    finally remove legacy guestbook examples

--- Beispiele/Node.js/GuestbookWebsocket/routes/indexRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// deliver ./public/index.html
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
-
-module.exports = router;
