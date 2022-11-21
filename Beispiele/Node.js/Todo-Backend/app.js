1462873519171fc331540f0b173e2e7894538d35
Sven Eppler <git@sveneppler.de>
2022-11-21 09:42:25 +0100

    implement basic in memory backend

---
+++ Beispiele/Node.js/Todo-Backend/app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+let todoInMemoryStore = [];
+
+// Alle Todos auslesen
+app.get("/api/v1/todos", (req, res) => {
+    return res.send(todoInMemoryStore);
+});
+
+// Todo speichern
+app.post("/api/v1/todos", (req, res) => {
+    var todoBody = req.body;
+    todoInMemoryStore.push(todoBody.todo);
+    return res.status(200).send();
+});
+
+// Todo löschen
+app.delete("/api/v1/todos", (req, res) => {
+    let todoBody = req.body;
+    let todoText = todoBody.todo;
+
+    todoInMemoryStore = todoInMemoryStore.filter(item => item != todoText);
+    return res.status(200).send();
+});
+
+module.exports = app;
