1bc21434d2cd3a6d9dab126df760de54c8f4efc5
Sven Eppler <git@sveneppler.de>
2023-05-05 09:48:27 +0200

    wip: todo-backend

--- Beispiele/Node.js/Todo-Backend/app.js
+++ Beispiele/Node.js/Todo-Backend/app.js
@@ -11,9 +11,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+let todoItemStore = [];
 
-app.get("/", (req,res) => {
-    return res.send("just an empty express app.");
+app.get("/api/v1/todos/", (req, res) => {
+    res.send(todoItemStore);
+});
+
+// POST new TodoItem as JSON
+// { "title": "yada", "id": 12, "created": "2023-04-24T12:00:00Z" }
+app.post("/api/v1/todos", (req, res) => {
+    const newTodoItem = req.body;
+    // TODO validate incoming data object
+    todoItemStore.push(newTodoItem);
+    res.status(200).send();
+});
+
+app.delete("/api/v1/todos/:todoId", (req, res) => {
+    const idToDelete = req.params.todoId;
+
+    todoItemStore = todoItemStore.filter((item) => {
+        return item.id != idToDelete;
+    });
+
+    res.status(200).send();
 });
 
 module.exports = app;
