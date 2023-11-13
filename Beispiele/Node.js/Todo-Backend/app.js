4c9d9f1b756f4468d7736a167a0f2c9d942c1f1f
Sven Eppler <git@sveneppler.de>
2023-11-13 10:29:54 +0100

    verbinden von frontend und backend

--- Beispiele/Node.js/Todo-Backend/app.js
+++ Beispiele/Node.js/Todo-Backend/app.js
@@ -11,6 +11,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Leere Platzhalteranwendung
+app.inMemoryStore = [];
+
+app.get("/todo-items/", (req, res) => {
+    res.send(app.inMemoryStore);
+});
+
+// Neues todo item hinzufügen
+app.post("/todo-items/", (req, res) => {
+    var todoItem = req.body;
+
+    app.inMemoryStore.push(todoItem);
+
+    res.status(200).send();
+});
+
+// Löschen eines Todo-Items
+app.delete("/todo-items/:id", (req, res) => {
+    const idToDelete = req.params.id;
+
+    app.inMemoryStore = app.inMemoryStore.filter(
+        (candiate) => {
+            return candiate.id !== idToDelete
+        }
+    );
+
+    return res.status(200).send();
+});
 
 module.exports = app;
